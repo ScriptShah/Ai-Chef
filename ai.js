466b568a33eb584a2c04581e844d6bafae2cdf6f
Ahmadshah Donishyar <ahmadshahdonishyar@gmail.com>
2025-12-02 09:21:59 +0430

    Ai api changed

--- ai.js
+++ ai.js
@@ -45,7 +45,7 @@ export async function getRecipeFromMistral(ingredientsArr) {
     const ingredientsString = ingredientsArr.join(", ")
     try {
         const response = await hf.chatCompletion({
-            model: "mistralai/Mixtral-8x7B-Instruct-v0.1",
+            model: "deepseek-ai/DeepSeek-V3.2",
             messages: [
                 { role: "system", content: SYSTEM_PROMPT },
                 { role: "user", content: `I have ${ingredientsString}. Please give me a recipe you'd recommend I make!` },
